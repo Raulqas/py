a28f1082f8e90e67d46f800f1c2ab4b33de0db2d
gasanovvv <89770304+Raulqas@users.noreply.github.com>
2023-05-15 14:19:48 +0500

    Add files via upload

---
+++ 9 lab prob/burger_pack/ingredients.py
@@ -0,0 +1,29 @@
+def cheese():
+    print('Сыр? Нормально')
+
+def bulochka():
+    print("Булочка? Давай")
+
+def no_vegan():
+    print("Котлета? Можно")
+
+def cucumber():
+    print("Огурец? Да")
+
+def tomato():
+    print("Помидор? ОК, давай")
+
+def ne_staroe_sovetskoe():
+    print("Маойнез? Нет, спасибо")
+
+def statoe_sovetsloe_sredstvo():
+    print("Помидоров? Можно")
+
+def statoe_sovetsloe_sredstvo1():
+    print("Кетчуп? Не откажусь")
+
+def statoe_sovetsloe_sredstvo2():
+    print("Сырный соус? Да")
+
+def statoe_sovetsloe_sredstvo3():
+    print("Лист салата? Конечно")
---
+++ 9 lab prob/burger_pack/my_burger.py
@@ -0,0 +1,23 @@
+from burger_pack.ingredients import *
+
+def cheeseburger():
+    print('Бургер из Западной страны')
+    bulochka()
+    cheese()
+    no_vegan()
+    ne_staroe_sovetskoe()
+    cheese()
+    bulochka()
+
+
+def shtosh():
+    print("Твое любимое")
+    bulochka()
+    cheese()
+    statoe_sovetsloe_sredstvo()
+    statoe_sovetsloe_sredstvo1()
+    statoe_sovetsloe_sredstvo2()
+    statoe_sovetsloe_sredstvo3()
+    no_vegan()
+    cheese()
+
